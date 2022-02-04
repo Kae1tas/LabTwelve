9d5888ede712dd4caf4e78e4ad1bf354fc9278a5
Mihail <79074705+Kae1tas@users.noreply.github.com>
2022-02-04 12:22:12 +0900

    1

--- src/Threads.java
+++ src/Threads.java
@@ -39,7 +39,7 @@ public class Threads {
             @Override
             public void run(){
                 for(int i = 0; i < HALF; i++){
-                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    a2[i] = (float) (a2[i] * Math.sin(0.2f + (i + HALF) / 5) * Math.cos(0.2f + (i + HALF) / 5) * Math.cos(0.4f + (i + HALF) / 2));
                 }
             }
         });
