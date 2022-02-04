940d5dd2bde2490503070aa3fe717ef1c0124136
Mihail <79074705+Kae1tas@users.noreply.github.com>
2022-02-04 10:37:34 +0900

    1

--- src/Threads.java
+++ src/Threads.java
@@ -44,29 +44,18 @@ public class Threads {
             }
         });
 
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
         System.arraycopy(a1, 0, arr, 0, HALF);
         System.arraycopy(a2, 0, arr, HALF, HALF);
 
-        Thread run1 = new Thread(new RunnableClass(a1, 0));
-        Thread run2 = new Thread(new RunnableClass(a2, HALF));
-
-        System.currentTimeMillis();
         System.out.println("Время работы метода:" + (System.currentTimeMillis() - a));
-    }
-}
-
-
-class RunnableClass implements Runnable{
-    private float [] array;
-    private int shift;
-    RunnableClass(float [] array, int shift){
-        this.array = array;
-        this.shift = shift;
-    }
-    @Override
-    public void run(){
-        for(int i = 0; i < array.length; i++){
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
     }
 }
